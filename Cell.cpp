4d12415050382c21b9ba16b4ef20bdd1c9f1d93c
Сергей Полевой <sergpolevoi@yandex.ru>
2021-11-02 19:33:31 +0400

    More constants~

--- Cell.cpp
+++ Cell.cpp
@@ -1,4 +1,5 @@
 ﻿#include "Cell.h"
+#include "Field.h"
 #include "framework.h"
 #include <stdlib.h>
 
@@ -7,3 +8,11 @@ const TCHAR* numbers[9] = { TEXT(" "), TEXT("1"), TEXT("2"), TEXT("3"),
 							TEXT("4"), TEXT("5"), TEXT("6"), TEXT("7"), TEXT("8") };
 const TCHAR* bomb = TEXT("💣");
 const TCHAR* flag = TEXT("🚩");
+const COLORREF colors[9] = { RGB(0, 0, 0), RGB(0, 0, 255), RGB(0, 255, 0), RGB(255, 0, 0),
+							 RGB(200, 0, 200), RGB(240, 240, 0), RGB(255, 165, 0), RGB(150, 0, 150), RGB(0, 0, 0) };
+const COLORREF odd_backgound = RGB(229, 194, 159);
+const COLORREF even_background = RGB(215, 184, 153);
+
+void draw_odd_backgournd(HDC hdc) {
+
+}
