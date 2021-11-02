987f26e73e4b28f19adf22be90c2bd5e1b8866a2
Сергей Полевой <sergpolevoi@yandex.ru>
2021-11-02 18:18:28 +0400

    Now you can distinguish correct coordinates

--- Source.cpp
+++ Source.cpp
@@ -3,17 +3,27 @@
 #define HEIGHT 14
 
 struct Field {
-	int actual[HEIGHT][WIDTH];
+	int cells[HEIGHT][WIDTH];
 	int opened[HEIGHT][WIDTH];
+	int width;
+	int height;
+	int bombs;
 };
 
-int check_field(Field* field) {
+int correct_coord(int row, int col) {
+	if (row >= 0 && row < HEIGHT && col >= 0 && col < WIDTH) {
+		return 1;
+	}
+	return 0;
+}
+
+int check_field(const Field* field) {
 	for (int x = 0; x < WIDTH; x++) {
 		for (int y = 0; y < HEIGHT; y++) {
-			if (field->opened[y][x] && field->actual[y][x] == -1) {
+			if (field->opened[y][x] && field->cells[y][x] == -1) {
 				return -1;
 			}
-			else if (!field->opened[y][x] && field->actual[y][x] != -1) {
+			else if (!field->opened[y][x] && field->cells[y][x] != -1) {
 				return 0;
 			}
 		}
@@ -22,9 +32,23 @@ int check_field(Field* field) {
 }
 
 int put_bomb(Field* field, int row, int col) {
-	if (row >= 0 && row < HEIGHT && col >= 0 && col < WIDTH) {
-		field->actual[row][col] = -1;
+	if (correct_coord(row, col)) {
+		field->cells[row][col] = -1;
 		return 0;
 	}
 	return -1;
+}
+
+int get_cell(Field* field, int row, int col) {
+	if (correct_coord(row, col)) {
+
+	}
+}
+
+int calculate_cells(Field* field) {
+	for (int x = 0; x < WIDTH; x++) {
+		for (int y = 0; y < HEIGHT; y++) {
+			field->cells[y][x] = field->cells[]
+		}
+	}
 }
