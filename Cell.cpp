a14f5abdf3fc1acbbc4bc96539f60f39d8e5187b
Сергей Полевой <sergpolevoi@yandex.ru>
2021-11-02 19:10:17 +0400

    Now there is text consts

---
+++ Cell.cpp
@@ -0,0 +1,9 @@
+﻿#include "Cell.h"
+#include "framework.h"
+#include <stdlib.h>
+
+
+const TCHAR* numbers[9] = { TEXT(" "), TEXT("1"), TEXT("2"), TEXT("3"), 
+							TEXT("4"), TEXT("5"), TEXT("6"), TEXT("7"), TEXT("8") };
+const TCHAR* bomb = TEXT("💣");
+const TCHAR* flag = TEXT("🚩");
